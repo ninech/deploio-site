b55e81e14876c104859015a512d895648af81dca
Daniel Bengl <daniel.bengl@renuo.ch>
2024-07-03 16:14:56 +0200

    Update badges

--- src/components/ui/badge/badge-variants.ts
+++ src/components/ui/badge/badge-variants.ts
@@ -1,11 +1,11 @@
 import { cva } from "class-variance-authority";
 
 export const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
     variants: {
       variant: {
-        default: "border-nine-gray-200 bg-nine-gray-50 text-nine-primary-900",
+        default: "bg-nine-primary text-white",
         secondary: "border-transparent bg-secondary text-secondary-foreground",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground",
